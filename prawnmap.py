4c0e089248ecd290dfaf126dad75e86d98969d85
kevin0418 <54438552+kevin0418@users.noreply.github.com>
2024-10-02 22:37:15 +1000

    Add location
    Add location  Bundaberg

--- prawnmap.py
+++ prawnmap.py
@@ -1,6 +1,14 @@
+
+
 import streamlit as st
+from streamlit_folium import folium_static
 import folium
 
 # 기본 지도 생성
-m = folium.Map(location=[37.5665, 126.9780], color='red', popup='Yong Sale', zoom_start=12)
-m.save('index.html')
+m = folium.Map(location=[-24.61992, 152.10412], zoom_start=12)
+
+# 마커 추가 (optional)
+folium.Marker(location=[-24.61992, 152.10412], popup='Yong Sale').add_to(m)
+
+# Streamlit에서 Folium 지도를 표시
+folium_static(m)
